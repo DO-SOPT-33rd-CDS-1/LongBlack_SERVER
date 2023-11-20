7d96c77b8e573e78c7b39915776a2dd1f7a4a53c
parkheeddong <phj7214go@gmail.com>
2023-11-20 19:30:34 +0900

    feat: Bookmark 엔티티 생성

---
+++ src/main/java/sopt/longBlack/domain/bookmark/Bookmark.java
@@ -0,0 +1,11 @@
+package sopt.longBlack.domain.bookmark;
+
+import jakarta.persistence.OneToOne;
+
+public class Bookmark {
+
+//    @OneToOne(mappedBy = "post_id")
+//    private Long postId;
+    private short bookmarkIdx;
+
+}
