efa8e55538b17f9c415fd2b2a54b20b083fc31fd
parkheeddong <phj7214go@gmail.com>
2023-11-20 22:25:15 +0900

    feat: Post 엔티티 생성

--- src/main/java/sopt/longBlack/domain/paragraph/Paragraph.java
+++ src/main/java/sopt/longBlack/domain/paragraph/Paragraph.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sopt.longBlack.domain.post.Post;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,14 +19,14 @@ import lombok.NoArgsConstructor;
 public class Paragraph {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private short paragraphId;
+    private Short paragraphId;
 
     private String content;
     private ParagraphType paragraphType;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "post_id")
-//    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 
     @Builder
     public Paragraph(String content, ParagraphType paragraphType) {
