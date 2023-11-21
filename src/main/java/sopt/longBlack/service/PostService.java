524b6aafb3fa53ded842442c7ba8febaaf40ecbf
parkheeddong <phj7214go@gmail.com>
2023-11-21 19:58:32 +0900

    feat: PostService 추가 및 getById 메소드 생성

--- src/main/java/sopt/longBlack/service/PostService.java
+++ src/main/java/sopt/longBlack/service/PostService.java
@@ -1,2 +1,24 @@
-package sopt.longBlack.service;public class PostService {
+package sopt.longBlack.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopt.longBlack.domain.post.Post;
+import sopt.longBlack.dto.response.SinglePostGetResponse;
+import sopt.longBlack.infrastructure.PostRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public SinglePostGetResponse getById(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new EntityNotFoundException("해당하는 포스트가 없습니다."));
+        return SinglePostGetResponse.of(post);
+    }
+
+
 }
