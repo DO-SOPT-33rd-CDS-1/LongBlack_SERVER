7232dc1761e9d2f597a16e4791e5fc10daebecdc
its-sky <smc9919@naver.com>
2023-11-21 12:32:40 +0900

    feat: Stamp 엔티티 생성

---
+++ src/main/java/sopt/longBlack/domain/stamp/Stamp.java
@@ -0,0 +1,25 @@
+package sopt.longBlack.domain.stamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import sopt.longBlack.domain.post.Post;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stampId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
