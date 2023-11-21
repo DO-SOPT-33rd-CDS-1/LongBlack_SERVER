e985c8af7e8429bf19972b2c03b8f8c83e14c610
its-sky <smc9919@naver.com>
2023-11-21 12:32:27 +0900

    feat: Likey 엔티티 생성

---
+++ src/main/java/sopt/longBlack/domain/likey/Likey.java
@@ -0,0 +1,25 @@
+package sopt.longBlack.domain.likey;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import sopt.longBlack.domain.post.Post;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Likey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeyId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
