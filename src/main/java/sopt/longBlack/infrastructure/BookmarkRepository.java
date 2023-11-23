985946ff460d80530bed21ce7361c1aba711b389
parkheeddong <phj7214go@gmail.com>
2023-11-23 18:15:08 +0900

    feat: Likey, Bookmark 레포지토리 추가

--- src/main/java/sopt/longBlack/infrastructure/BookmarkRepository.java
+++ src/main/java/sopt/longBlack/infrastructure/BookmarkRepository.java
@@ -1,7 +1,10 @@
 package sopt.longBlack.infrastructure;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.longBlack.domain.bookmark.Bookmark;
+import sopt.longBlack.domain.post.Post;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Optional<Bookmark> findByPost(Post post);
 }
--- src/main/java/sopt/longBlack/infrastructure/LikeyRepository.java
+++ src/main/java/sopt/longBlack/infrastructure/LikeyRepository.java
@@ -3,6 +3,8 @@ package sopt.longBlack.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.longBlack.domain.bookmark.Bookmark;
 import sopt.longBlack.domain.likey.Likey;
+import sopt.longBlack.domain.post.Post;
 
 public interface LikeyRepository extends JpaRepository<Likey, Long> {
+    boolean existsByPost(Post post);
 }
