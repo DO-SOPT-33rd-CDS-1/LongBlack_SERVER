7bfa275c7407a5658624fb69e6acb7176672e14a
parkheeddong <phj7214go@gmail.com>
2023-11-23 18:20:13 +0900

    feat: stamp repository 추가

--- src/main/java/sopt/longBlack/infrastructure/StampRepository.java
+++ src/main/java/sopt/longBlack/infrastructure/StampRepository.java
@@ -2,7 +2,9 @@ package sopt.longBlack.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.longBlack.domain.bookmark.Bookmark;
+import sopt.longBlack.domain.post.Post;
 import sopt.longBlack.domain.stamp.Stamp;
 
 public interface StampRepository extends JpaRepository<Stamp, Long> {
+    boolean existsByPost(Post post);
 }
