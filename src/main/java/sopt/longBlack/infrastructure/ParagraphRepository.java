ae111208c175df7f8bf8adcff0e974dc648a5280
parkheeddong <phj7214go@gmail.com>
2023-11-23 18:17:48 +0900

    feat: 단일 게시물 조회 서비스 로직 구현

--- src/main/java/sopt/longBlack/infrastructure/ParagraphRepository.java
+++ src/main/java/sopt/longBlack/infrastructure/ParagraphRepository.java
@@ -1,7 +1,10 @@
 package sopt.longBlack.infrastructure;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.longBlack.domain.paragraph.Paragraph;
+import sopt.longBlack.domain.post.Post;
 
 public interface ParagraphRepository extends JpaRepository<Paragraph, Short> {
+    List<Paragraph> findAllByPost(Post post);
 }
--- src/main/java/sopt/longBlack/service/PostService.java
+++ src/main/java/sopt/longBlack/service/PostService.java
@@ -1,12 +1,20 @@
 package sopt.longBlack.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.longBlack.domain.bookmark.Bookmark;
+import sopt.longBlack.domain.paragraph.Paragraph;
 import sopt.longBlack.domain.post.Post;
 import sopt.longBlack.dto.response.SinglePostGetResponse;
+import sopt.longBlack.infrastructure.BookmarkRepository;
+import sopt.longBlack.infrastructure.LikeyRepository;
+import sopt.longBlack.infrastructure.ParagraphRepository;
 import sopt.longBlack.infrastructure.PostRepository;
+import sopt.longBlack.infrastructure.StampRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +22,28 @@ import sopt.longBlack.infrastructure.PostRepository;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final LikeyRepository likeyRepository;
+    private final StampRepository stampRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final ParagraphRepository paragraphRepository;
 
     public SinglePostGetResponse getById(Long postId) {
+        // post 정보 (postId, title, writer, createdDate, postType)
         Post post = postRepository.findById(postId).orElseThrow(() -> new EntityNotFoundException("해당하는 포스트가 없습니다."));
-        return SinglePostGetResponse.of(post);
+        // like, stamp의 존재 여부 확인
+        boolean like = likeyRepository.existsByPost(post);
+        boolean stamp = stampRepository.existsByPost(post);
+        // bookmark 정보 확인
+//        Bookmark bookmark = bookmarkRepository.findByPost(post);
+//        Integer bookmarkIdx = null;
+//        if (bookmark != null ) {
+//            bookmarkIdx = bookmark.getBookmarkIdx();
+//        }
+        Optional<Bookmark> bookmarkOptional = bookmarkRepository.findByPost(post);
+        Integer bookmarkIdx = bookmarkOptional.map(Bookmark::getBookmarkIdx).orElse(null);
+        // paragraph 정보 확인
+        List<Paragraph> paragraphs = paragraphRepository.findAllByPost(post);
+        return SinglePostGetResponse.of(post, like, stamp, bookmarkIdx, paragraphs);
     }
 
 
