b35cad8644e39c4e371af81bf546eb70432d7487
parkheeddong <phj7214go@gmail.com>
2023-11-23 18:19:26 +0900

    feat: 게시물 response dto 구조 변경

--- src/main/java/sopt/longBlack/dto/response/SinglePostGetResponse.java
+++ src/main/java/sopt/longBlack/dto/response/SinglePostGetResponse.java
@@ -4,15 +4,30 @@ import java.time.LocalDate;
 import java.util.List;
 import sopt.longBlack.domain.paragraph.Paragraph;
 import sopt.longBlack.domain.post.Post;
+import sopt.longBlack.domain.post.PostType;
 
-public record SinglePostGetResponse(Long id, String title, String writer, LocalDate createdDate) {
+public record SinglePostGetResponse(Long id,
+                                    String title,
+                                    String writer,
+                                    LocalDate createdDate,
+                                    PostType postType,
+                                    boolean like,
+                                    boolean stamp,
+                                    Integer bookmarkIdx,
+                                    List<Paragraph> paragraphs
+) {
 
-    public static SinglePostGetResponse of(Post post) {
+    public static SinglePostGetResponse of(Post post, boolean like, boolean stamp, Integer bookmarkIdx, List<Paragraph> paragraphs) {
         return new SinglePostGetResponse(
                 post.getPostId(),
                 post.getTitle(),
                 post.getWriter(),
-                post.getCreatedDate()
+                post.getCreatedDate(),
+                post.getPostType(),
+                like,
+                stamp,
+                bookmarkIdx,
+                paragraphs
         );
     };
 
