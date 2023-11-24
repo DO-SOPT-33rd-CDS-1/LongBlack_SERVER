f2a05eec9bcf21ac4d77e1ae8db1f7c7f8fedf8c
parkheeddong <phj7214go@gmail.com>
2023-11-24 14:49:18 +0900

    feat: bookmark Service 추가

--- src/main/java/sopt/longBlack/service/BookmarkService.java
+++ src/main/java/sopt/longBlack/service/BookmarkService.java
@@ -1,2 +1,33 @@
-package sopt.longBlack.service;public class BookmarkService {
+package sopt.longBlack.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopt.longBlack.domain.bookmark.Bookmark;
+import sopt.longBlack.domain.post.Post;
+import sopt.longBlack.dto.request.BookmarkRequest;
+import sopt.longBlack.infrastructure.BookmarkRepository;
+import sopt.longBlack.infrastructure.PostRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public String postBookMark(BookmarkRequest request, Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new EntityNotFoundException("해당하는 포스트가 없습니다."));
+
+        Bookmark bookmark = bookmarkRepository.save(
+                Bookmark.builder()
+                        .bookmarkIdx(request.bookmarkIdx())
+                        .post(post).build());
+
+        return bookmark.getBookmarkId().toString();
+
+    }
 }
